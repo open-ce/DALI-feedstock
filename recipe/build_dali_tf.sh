b78fe812839f6aaccefaae7377ec28339dc718b3
Nishidha Panpaliya <npanpa23@in.ibm.com>
2020-12-02 17:49:15 +0000

    Refreshed DALI to 0.28

--- recipe/build_dali_tf.sh
+++ recipe/build_dali_tf.sh
@@ -18,8 +18,15 @@ LIB_NAME=${1:-"libdali_tf_current.so"}
 SRCS="daliop.cc dali_dataset_op.cc"
 INCL_DIRS="-I$CUDA_HOME/include -I$CONDA_PREFIX/include/"
 
-DALI_CFLAGS=( $($PYTHON ./dali_compile_flags.py --cflags) )
-DALI_LFLAGS=( $($PYTHON ./dali_compile_flags.py --lflags) )
+DALI_STUB_DIR=`mktemp -d -t "dali_stub_XXXXXX"`
+DALI_STUB_SRC="${DALI_STUB_DIR}/dali_stub.cc"
+DALI_STUB_LIB="${DALI_STUB_DIR}/libdali.so"
+$PYTHON ../tools/stubgen.py ../include/dali/c_api.h --output "${DALI_STUB_SRC}"
+
+DALI_CFLAGS="-I../include"
+DALI_LFLAGS="-L${DALI_STUB_DIR} -ldali"
+
+$COMPILER -std=c++11 -DNDEBUG -O2 -shared -fPIC ${DALI_STUB_SRC} -o ${DALI_STUB_LIB} ${INCL_DIRS} ${DALI_CFLAGS}
 
 TF_CFLAGS=( $($PYTHON -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
 TF_LFLAGS=( $($PYTHON -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
