f7b6cd804ee2af5e04fb6d3ad948e37f797dac7e
Aman Surkar <Aman.Surkar@ibm.com>
2024-05-07 12:56:31 +0000

    Added fix for cuda 11.8 failure

--- recipe/build-dali.sh
+++ recipe/build-dali.sh
@@ -16,10 +16,14 @@
 # *****************************************************************
 
 # Determine Architecture
+set -ex
 
 ARCH="$(arch)"
 if [ ${ARCH} = "x86_64" ]; then
     ARCH_LONGNAME="x86_64-conda"
+    if [[ ${cudatoolkit} == "11.8" ]]; then
+        export BUILD_CVCUDA=OFF
+    fi
 elif [ ${ARCH} = "ppc64le" ]; then
     ARCH_LONGNAME="powerpc64le-conda"
     export BUILD_CVCUDA=OFF
@@ -28,6 +32,7 @@ else
     exit 1
 fi
 
+echo $BUILD_CVCUDA
 # Create 'gcc' symlink so nvcc can find it
 ln -s $CONDA_PREFIX/bin/${ARCH_LONGNAME}-linux-gnu-gcc $CONDA_PREFIX/bin/gcc
 ln -s $CONDA_PREFIX/bin/${ARCH_LONGNAME}-linux-gnu-g++ $CONDA_PREFIX/bin/g++
@@ -99,9 +104,11 @@ make -j"$(nproc --all)" install
 
 #Copy for x86 alone, as cv-cuda is disabled for ppc.
 if [ ${ARCH} = "x86_64" ]; then
-  #libs created by cvcuda not getting installed to PREFIX
-  cp $SRC_DIR/build/lib/libcv* $PREFIX/lib
-  cp $SRC_DIR/build/lib/libnvcv_types* $PREFIX/lib
+  if [[ ${cudatoolkit} != "11.8" ]]; then
+      #libs created by cvcuda not getting installed to PREFIX
+      cp $SRC_DIR/build/lib/libcv* $PREFIX/lib
+      cp $SRC_DIR/build/lib/libnvcv_types* $PREFIX/lib
+  fi
 fi
 
 export PYTHONUSERBASE=$PREFIX
